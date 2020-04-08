e1bb367244e21557475c0bb842f76d9eadbc128e
RubenG-tech <qbitechnology@gmail.com>
2020-04-08 16:17:57 -0300

    eclinic commit

---
+++ resources/views/includes/panel/footer.blade.php
@@ -0,0 +1,19 @@
+<footer class="footer">
+  <div class="row align-items-center justify-content-xl-between">
+    <div class="col-xl-6">
+      <div class="copyright text-center text-xl-left text-muted">
+        &copy; 2020 <a href="Hecho con amor por nosotros 3" class="font-weight-bold ml-1" target="_blank">{{ config('app.name') }}</a>
+      </div>
+    </div>
+    <div class="col-xl-6">
+      <ul class="nav nav-footer justify-content-center justify-content-xl-end">
+        <li class="nav-item">
+          <a href="" class="nav-link" target="_blank">contactanos</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link" target="_blank">Acerca de nosotros</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</footer>
